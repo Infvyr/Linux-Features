3164f8d245b29abf85c191508972bac29bff0df3
Infvyr <allfortrashmail@gmail.com>
2019-02-24 21:46:08 +0200

    Updated radio.sh

--- radio.sh
+++ radio.sh
@@ -12,7 +12,8 @@ echo "<********************************>"
 echo "1. Open MuzFM Chisinau"
 echo "2. Open ZaycevFM Relax"
 echo "3. Open ZaycevFM New Rock"
-echo "4. Exit Script"
+echo "4. Open ZaycevFM Disco"
+echo "5. Exit Script"
 echo "<********************************>"
 read -r -p "Enter your choice [1-4] : " option
 
@@ -20,7 +21,8 @@ case $option in
 1) echo 'Starting MuzFM...: ';  mpv http://live.muzfm.md:8000/muzfm;;
 2) echo 'Starting ZaycevFM Relax...: ';  mpv https://zaycevfm.cdnvideo.ru/ZaycevFM_relax_256.mp3;;
 3) echo 'Starting ZaycevFM New Rock'; mpv https://zaycevfm.cdnvideo.ru/ZaycevFM_rock_256.mp3;;
-4) exit;;
-*) echo -e "\e[033mSelect between 1 to 4 only\n\033[0m"
+4) echo 'Starting ZaycevFM Disco'; mpv https://www.zaycev.fm/media/m3u/chanel_electronic.m3u;;
+5) exit;;
+*) echo -e "\e[033mJust select between 1 and 5\n\033[0m"
 esac
 done
