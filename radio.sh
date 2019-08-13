ed611ee5d7e6072d8600a091ab03ae91336ce1b0
Infvyr <allfortrashmail@gmail.com>
2019-08-13 22:19:08 +0300

    added one more radio station

--- radio.sh
+++ radio.sh
@@ -13,8 +13,9 @@ echo "1. Open MuzFM Chisinau"
 echo "2. Open ZaycevFM Relax"
 echo "3. Open ZaycevFM New Rock"
 echo "4. Open ZaycevFM Club"
-echo "5. Open ZaycevFM Club"
-echo "6. Exit Script"
+echo "5. Open Concept Radio"
+echo "6. Open Rock Mixx"
+echo "7. Exit Script"
 echo "<********************************>"
 read -r -p "Enter your choice [1-6] : " option
 
@@ -24,7 +25,8 @@ case $option in
 3) echo 'Starting ZaycevFM New Rock'; mpv https://zaycevfm.cdnvideo.ru/ZaycevFM_rock_256.mp3;;
 4) echo 'Starting ZaycevFM Club'; mpv --playlist=https://www.zaycev.fm/media/m3u/chanel_electronic.m3u;;
 5) echo 'Starting Concept Radio'; mpv http://manager.conceptradio.fr:1460/stream;;
-6) exit;;
+6) echo 'Starting Rock Mixx'; http://113fm-edge2.cdnstream.com/5165_128;;
+7) exit;;
 *) echo -e "\e[033mJust select between 1 and 6\n\033[0m"
 esac
 done
